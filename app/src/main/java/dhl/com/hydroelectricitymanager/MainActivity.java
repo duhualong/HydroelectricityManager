065a614481ba8beb544b2c6e3cfc39f00037eb8c
du <1456198324@qq.com>
2016-03-24 18:38:09 +0800

    Ui Reservation

--- app/src/main/java/dhl/com/hydroelectricitymanager/MainActivity.java
+++ app/src/main/java/dhl/com/hydroelectricitymanager/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service_address);
+        setContentView(R.layout.order_confirm);
     }
 }
